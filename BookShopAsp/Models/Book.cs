3aff995507a067050e85bd694dc965af80d9faa0
Рита Матвеева <rmatveeva1@gmail.com>
2023-03-08 23:11:26 +0300

    Добавьте файлы проекта.

---
+++ BookShopAsp/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopAsp.Models
+{
+    public class Book
+    {
+         // ID книги
+            public int Id { get; set; }
+            // название книги
+            [Required]
+            [Display(Name = "Название")]
+            public string Name { get; set; }
+            // автор книги
+            [Required]
+            [Display(Name = "Автор")]
+            public string Author { get; set; }
+
+            [Display(Name = "Жанр")]
+            public string Genre { get; set; }
+            // цена
+            [Required]
+            [Display(Name = "Цена")]
+            public int Price { get; set; }
+            public List<Order> Orders { get; set; } = new List<Order>();
+        
+    }
+}
+
---
+++ BookShopAsp/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+namespace BookShopAsp.Models
+{
+    public class Order
+    {
+            // ID покупки
+            public int Id { get; set; }
+            // имя и фамилия покупателя
+            [Required]
+            [Display(Name = "Покупатель")]
+            public string UserId { get; set; }
+            public IdentityUser? User { get; set; }
+            // адрес покупателя
+            [Required]
+            [Display(Name = "Адрес")]
+            public string Address { get; set; }
+            // ID книги
+            [Required, Display(Name = "Книга")]
+            public int BookId { get; set; }
+            public Book? Book { get; set; }
+            // дата покупки
+            [DataType(DataType.Date), Display(Name ="Дата")]
+            public DateTime Date { get; set; }
+        }
+}
